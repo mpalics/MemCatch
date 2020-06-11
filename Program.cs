4627dfff3373a50111902c8739dc8d93bd974b50
Marcell Pálics <marcell.palics@icloud.com>
2020-06-11 18:12:48 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace MemCatcher
+{
+    class Program
+    {
+        // REQUIRED CONSTS
+        const int PROCESS_QUERY_INFORMATION = 0x0400;
+        const int MEM_COMMIT = 0x00001000;
+        const int PAGE_READWRITE = 0x04;
+        const int PROCESS_WM_READ = 0x0010;
+
+        // REQUIRED METHODS
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, long lpBaseAddress, byte[] lpBuffer, long dwSize, ref int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
+
+        // REQUIRED STRUCTS
+        public enum AllocationProtect : uint
+        {
+            PAGE_EXECUTE = 0x00000010,
+            PAGE_EXECUTE_READ = 0x00000020,
+            PAGE_EXECUTE_READWRITE = 0x00000040,
+            PAGE_EXECUTE_WRITECOPY = 0x00000080,
+            PAGE_NOACCESS = 0x00000001,
+            PAGE_READONLY = 0x00000002,
+            PAGE_READWRITE = 0x00000004,
+            PAGE_WRITECOPY = 0x00000008,
+            PAGE_GUARD = 0x00000100,
+            PAGE_NOCACHE = 0x00000200,
+            PAGE_WRITECOMBINE = 0x00000400
+        }
+
+        public struct MEMORY_BASIC_INFORMATION
+        {
+            public IntPtr BaseAddress;
+            public IntPtr AllocationBase;
+            public uint AllocationProtect;
+            public IntPtr RegionSize;
+            public uint State;
+            public uint Protect;
+            public uint Type;
+        }
+
+        public struct MEMORY_BASIC_INFORMATION64
+        {
+            public IntPtr BaseAddress;
+            public IntPtr AllocationBase;
+            public int AllocationProtect;
+            public int __alignment1;
+            public IntPtr RegionSize;
+            public int State;
+            public int Protect;
+            public int Type;
+            public int __alignment2;
+        }
+
+        public struct SYSTEM_INFO
+        {
+            public ushort processorArchitecture;
+            ushort reserved;
+            public uint pageSize;
+            public IntPtr minimumApplicationAddress;
+            public IntPtr maximumApplicationAddress;
+            public IntPtr activeProcessorMask;
+            public uint numberOfProcessors;
+            public uint processorType;
+            public uint allocationGranularity;
+            public ushort processorLevel;
+            public ushort processorRevision;
+        }
+
+        // finally...
+        public static void Main()
+        {
+            // getting minimum & maximum address
+            SYSTEM_INFO sys_info = new SYSTEM_INFO();
+            GetSystemInfo(out sys_info);
+
+            IntPtr proc_min_address = sys_info.minimumApplicationAddress;
+            IntPtr proc_max_address = sys_info.maximumApplicationAddress;
+
+            // saving the values as long ints so I won't have to do a lot of casts later
+            long proc_min_address_l = (long)proc_min_address;
+            long proc_max_address_l = (long)proc_max_address;
+
+            // notepad better be runnin'
+            Process process = Process.GetProcessesByName("notepad")[0];
+            Console.Write("String: ");
+            string searchstr = Console.ReadLine();
+
+            // opening the process with desired access level
+            IntPtr processHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_WM_READ, false, process.Id);
+
+            List<Tuple<string, char>> searchbuffer = new List<Tuple<string, char>>();
+
+            StreamWriter sw = new StreamWriter("mem_dump.txt");
+            StreamWriter sw2 = new StreamWriter("found.txt");
+
+            // this will store any information we get from VirtualQueryEx()
+            MEMORY_BASIC_INFORMATION mem_basic_info = new MEMORY_BASIC_INFORMATION();
+
+            uint mbi_size;
+            unsafe
+            {
+                mbi_size = (uint)sizeof(MEMORY_BASIC_INFORMATION);
+            }
+            
+
+            int bytesRead = 0;  // number of bytes read with ReadProcessMemory
+
+            while (proc_min_address_l < proc_max_address_l)
+            {
+                // 28 = sizeof(MEMORY_BASIC_INFORMATION)
+                VirtualQueryEx(processHandle, proc_min_address, out mem_basic_info, mbi_size);
+
+                Console.Write(proc_min_address_l + " / " + proc_max_address_l);
+                Console.SetCursorPosition(0, 0);
+                // if this memory chunk is accessible
+                if (mem_basic_info.Protect == PAGE_READWRITE && mem_basic_info.State == MEM_COMMIT)
+                {
+                    byte[] buffer = new byte[(ulong)mem_basic_info.RegionSize];
+
+                    // read everything in the buffer above
+                    ReadProcessMemory((int)processHandle, (long)mem_basic_info.BaseAddress, buffer, (long)mem_basic_info.RegionSize, ref bytesRead);
+
+                    // then output this in the file
+                    for (int i = 0; i < (long)mem_basic_info.RegionSize; i++)
+                    {
+                        if ((char)buffer[i] != '\0')
+                        {
+                            sw.WriteLine("0x{0} : {1}", (mem_basic_info.BaseAddress + i).ToString("X"), (char)buffer[i]);
+                            searchbuffer.Add(new Tuple<string, char>(
+                                String.Format("0x{0}", (mem_basic_info.BaseAddress + i).ToString("X")),
+                                (char)buffer[i]));
+                        }
+
+                        //
+                        if (new String(searchbuffer.Select(s => s.Item2).ToArray()).Equals(searchstr))
+                        {
+                            foreach (var chunk in searchbuffer)
+                            {
+                                sw2.WriteLine("0x{0} : {1}", chunk.Item1, chunk.Item2);
+                            }
+                        }
+
+                        if (searchbuffer.Count >= searchstr.Length)
+                        {
+                            searchbuffer.Clear();
+                        }
+                    }
+                }
+
+                // move to the next memory chunk
+                proc_min_address_l += (long)mem_basic_info.RegionSize;
+                proc_min_address = new IntPtr(proc_min_address_l);
+            }
+            sw.Close();
+            sw2.Close();
+        }
+    }
+}
